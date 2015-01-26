c89234eb173ad9cc21489154953f3768179b22a1
Phil <marshall.phillip@gmail.com>
2015-01-26 00:09:39 -0600

    some commit message

---
+++ game.js
@@ -0,0 +1,83 @@
+;(function() {
+ var Game = function(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    var screen = canvas.getContext('2d');
+    var gameSize = { x: canvas.width, y: canvas.height};
+
+    this.bodies = [new Player(this, gameSize )]
+
+    var self = this;
+
+    var tick = function() {
+        self.update();
+        self.draw(screen, gameSize);
+        requestAnimationFrame(tick);
+    };
+
+    tick();
+ };
+
+ Game.prototype = {
+    update: function() {
+        for (var i = 0; i < this.bodies.length; i++) {
+          this.bodies[i].update();
+       }
+    },
+
+    draw: function(screen, gameSize) {
+        screen.clearRect(0, 0, gameSize.x, gameSize.y);
+        for (var i = 0; i < this.bodies.length; i++) {
+            drawRect(screen, this.bodies[i]);
+        }
+    }
+};
+
+var Player = function(game, gameSize) {
+    this.game = game;
+    this.size = { x: 15, y: 15 };
+    this.center = { x: gameSize.x / 2, y: gameSize.y - this.size.x };
+    this.keyboarder = new Keyboarder();
+};
+
+Player.prototype = {
+    update: function() {
+        if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+            this.center.x -= 2;
+        } else if (this.keyboarder.isDown(this.eyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+    }
+
+   }
+};
+
+var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+                    body.center.y - body.size.y / 2,
+                    body.size.x, body.size.y);
+};
+
+var Keyboarder = function() {
+    var keyState = {};
+
+    window.onekeydown = function(e) {
+        keyState[e.keCode] = true;
+    };
+
+    window.onekeyup = function(e) {
+        keyState[e.keyCode] = false;
+    };
+
+    this.isDown = function (keyCode) {
+        return keyState[keyCode] === true;
+    };
+
+    this.Keys = { LEFT: 37, RIGHT: 39, SPACE: 32};
+window.onload = function() {
+    new Game("screen");
+};
+
+};
+
+
+
+
